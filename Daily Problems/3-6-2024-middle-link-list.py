a4ccae6d89b02f17ada19c8aee3f3fb702fb7b6b
William-Sinn <sinnwilliam7@gmail.com>
2024-03-07 00:05:59 -0800

    3/6/2024 Daily End - 72% Runtime | 92% Memory

--- Daily Problems/3-6-2024-middle-link-list.py
+++ Daily Problems/3-6-2024-middle-link-list.py
@@ -9,4 +9,15 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        
+        curr_node = head
+        node_list = []
+
+        while curr_node != None:
+            node_list.append(curr_node)
+
+            curr_node = curr_node.next
+
+        list_len = len(node_list) - 1
+        mid_node = (list_len // 2) + (list_len % 2)
+
+        return(node_list[mid_node])
