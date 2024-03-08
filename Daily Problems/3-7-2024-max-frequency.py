2c3b5ae985e07aae676084f2c4e76dda18a479c3
William-Sinn <sinnwilliam7@gmail.com>
2024-03-07 18:13:15 -0800

    3/7/2024 First Solution - Runtime 97% | Memory 91%

--- Daily Problems/3-7-2024-max-frequency.py
+++ Daily Problems/3-7-2024-max-frequency.py
@@ -4,4 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        max_frq = 0
+        total_frq = 0
+        num_dict = {}
+
+        for num in nums:
+
+            if num in num_dict:
+                num_dict[num] += 1
+            else:
+                num_dict[num] = 1
+                
+            if num_dict[num] > max_frq:
+                max_frq = num_dict[num]
+                total_frq = 1
+            elif num_dict[num] == max_frq:
+                    total_frq += 1
         
+        return(max_frq * total_frq)
