72657ca101df720c008c0e2a009098e56958c6ab
William-Sinn <sinnwilliam7@gmail.com>
2024-03-25 19:47:34 -0700

    3/25/24 Final Attempt - Runtime 99% | Memory 19%

--- Daily Problems/3-25-24-first-missing-positive.py
+++ Daily Problems/3-25-24-first-missing-positive.py
@@ -1,15 +1,7 @@
 class Solution(object):
     def firstMissingPositive(self, nums):
-        num_length = len(nums)
+        nums = set(nums)
 
-        for num in nums:
-            index = num - 1
-            if index >= 0 and index < num_length:
-                int_index = int(index)
-                nums[int_index] = float(nums[int_index])
-
-        for i in range(num_length):
-            if isinstance(nums[i], int):
-                return i + 1
-
-        return num_length + 1
+        for x in range(1, len(nums) + 2):
+            if x not in nums:
+                return x
