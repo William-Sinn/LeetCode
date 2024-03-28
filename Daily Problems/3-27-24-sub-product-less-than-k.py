8024bba46c843774a16bde28f8009aa07b0b0c72
William-Sinn <sinnwilliam7@gmail.com>
2024-03-27 20:20:01 -0700

    3/27/24 Final  Solution - Runtime 94% | Memory 33%

--- Daily Problems/3-27-24-sub-product-less-than-k.py
+++ Daily Problems/3-27-24-sub-product-less-than-k.py
@@ -9,47 +9,19 @@ class Solution(object):
         :rtype: int
         """
 
-        start_i = 0
-        end_i = 0
-        arr_len = len(nums)
+        if k <= 1:
+            return 0
+        
+        res = 0
+        pro = 1
+        left = 0
+        for a,b in enumerate(nums):
+            pro *= b
 
-        p = 1
-        while start_i < arr_len:
-            p *= nums[end_i]
+            while pro >= k:
+                pro /= nums[left]
+                left += 1
 
-            if p >= k: 
-                p = p/nums[end_i]
+            res += (a-left) + 1
 
-                if start_i == end_i:
-                    end_i += 1
-                    start_i += 1
-                    continue
-
-
-                if p < k:
-                    self.valid_subs += end_i - start_i
-                    p = p/nums[start_i]
-                
-                    
-                start_i +=1
-
-
-
-            elif end_i + 1 == arr_len:
-
-                p = p/nums[start_i]
-
-                if p < k:
-                    self.valid_subs += end_i + 1 - start_i
-                    
-                start_i +=1
-            
-                p = p/nums[end_i]
-
-            else:
-                end_i += 1
-
-            
-
-
-        return self.valid_subs
+        return res
