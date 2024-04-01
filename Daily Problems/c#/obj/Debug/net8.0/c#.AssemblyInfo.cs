c2d9ee07fe4f4b7fafc322fe4783c8b37ac5a2d4
William-Sinn <sinnwilliam7@gmail.com>
2024-04-01 08:24:46 -0700

    Test Problem

--- Daily Problems/c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ Daily Problems/c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5cc1222d0057d0b11c83b354027d47263b85939")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86cc10ccec2ef8d841e0e1ed58b3d0bf5484f514")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Daily Problems/c#/twoSum.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int i = 0;
+        int j; 
+        
+        foreach (int num in nums) {
+            j = 0;
+            foreach (var num2 in nums) {
+                if (i == j) {}
+                else if (num + num2 == target) {
+                    return [i, j];
+                } 
+                j++;
+            }
+            i++;
+        }
+        return [];
+    }
+}
