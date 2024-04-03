286f92453202dc7d7619d1dceaf776bf298648e7
William-Sinn <sinnwilliam7@gmail.com>
2024-04-02 17:16:09 -0700

    update gitignore

--- Daily Problems/c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ Daily Problems/c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2d9ee07fe4f4b7fafc322fe4783c8b37ac5a2d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f668dab2e40283ecb23d09094612d118451a4e79")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
