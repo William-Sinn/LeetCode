ba2e22794a6a47837da72a062592b319c16c48bb
William-Sinn <sinnwilliam7@gmail.com>
2024-04-10 17:24:55 -0700

    4/10/24 Final Solution - Runtime 96% | Memory 68%

--- Daily Problems/python/4-10-24-reveal-deck-increasing.py
+++ Daily Problems/python/4-10-24-reveal-deck-increasing.py
@@ -1,8 +1,19 @@
+from collections import deque
+
 class Solution(object):
     def deckRevealedIncreasing(self, deck):
         """
         :type deck: List[int]
         :rtype: List[int]
         """
+        d_len = len(deck)
+        out_deck = [None] * d_len
+        index_arr = deque(range(d_len))
+
+        for card in sorted(deck):
+            out_deck[index_arr.popleft()] = card
+
+            if index_arr:
+                index_arr.append(index_arr.popleft())
         
-        
+        return out_deck
