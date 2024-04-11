6cf7808d1fb30778224f02bf6c19128abc5753f2
William-Sinn <sinnwilliam7@gmail.com>
2024-04-10 19:37:16 -0700

    4/11/24 Final Solution - Runtime 99% | Memory 83%

--- Daily Problems/python/4-11-24-remove-k-digits.py
+++ Daily Problems/python/4-11-24-remove-k-digits.py
@@ -1,8 +1,24 @@
 class Solution(object):
     def removeKdigits(self, num, k):
-        """
-        :type num: str
-        :type k: int
-        :rtype: str
-        """
+        if len(num) == k:
+            return "0"
         
+        mono_inc = []
+        for i, n in enumerate(num):
+            if not mono_inc or n >= mono_inc[-1]:
+                mono_inc.append(n)
+            else:
+                while mono_inc and n < mono_inc[-1]:
+                    mono_inc.pop()
+                    k -= 1
+                    if k == 0:
+                        res = ''.join(mono_inc)+num[i:]
+                        while len(res)>1 and res[0] == '0':
+                            res = res[1:]
+                        return res
+                mono_inc.append(n)   
+                
+        res = ''.join(mono_inc[:-k])
+        while len(res)>1 and res[0] == '0':
+            res = res[1:]
+        return res
