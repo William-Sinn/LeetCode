c8248e186dcbe2eea0f51877a1f4646007c69853
William-Sinn <sinnwilliam7@gmail.com>
2024-07-26 18:03:53 -0700

    Daily Problem 07/26/24 1 - Submission 1 Corrected

--- Grok/Sliding Window/Hard/max-fruits-k-steps.py
+++ Grok/Sliding Window/Hard/max-fruits-k-steps.py
@@ -13,8 +13,9 @@ class Solution(object):
         right_pre = 0
         left_pre = 0
         path = right_path
-        left = startPos 
-        right = startPos
+        sec_path = left_path
+        left = [startPos]
+        right = [startPos]
         prim_dir = right
         sec_dir = left
         i = 1
@@ -26,7 +27,7 @@ class Solution(object):
                     right_pre += fruit[1]
 
             elif fruit[0] == startPos:
-                fruits += fruits[1]
+                max_fruits += fruit[1]
 
             else:
                 if fruit[0] >= startPos - k:
@@ -35,27 +36,32 @@ class Solution(object):
 
         if left_pre > right_pre:
             path = left_path
+            sec_path = right_path
             prim_dir = left
             sec_dir = right
             i = -1
 
         if (len(right_path) or len(left_path)):
-            while abs(left - right) < k:
-                prim_dir += i
-                if prim_dir in path:
-                    max_fruits += path[prim_dir]
+            while abs(left[0] - right[0]) < k and prim_dir[0] >= 0:
+                prim_dir[0] += i
+                if prim_dir[0] in path:
+                    max_fruits += path[prim_dir[0]]
 
             curr_fruits = max_fruits
 
-            while prim_dir != startPos:
-                if prim_dir in path:
-                    curr_fruits -= path[prim_dir]
-                prim_dir += -i
+            if prim_dir[0] in path:
+                curr_fruits -= path[prim_dir[0]]
 
-                sec_dir += -i
-                if sec_dir in path:
-                    curr_fruits += path[prim_dir]
+            while prim_dir[0] != startPos and sec_dir[0] >= 0:
+                prim_dir[0] += -i
+                if prim_dir[0] in path:
+                    curr_fruits -= path[prim_dir[0]]
+
+                sec_dir[0] += -i
+                if sec_dir[0] in sec_path:
+                    curr_fruits += sec_path[sec_dir[0]]
                 
                 max_fruits = max(max_fruits, curr_fruits)
 
-        return fruits
+
+        return max_fruits
