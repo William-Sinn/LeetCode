4c5cd0fe361d2afe2da2ef1cf7e6102ce9086b31
William-Sinn <sinnwilliam7@gmail.com>
2024-07-26 23:25:51 -0700

    Daily Problem 07/26/24 1 - Final Submission

--- Grok/Sliding Window/Hard/max-fruits-k-steps.py
+++ Grok/Sliding Window/Hard/max-fruits-k-steps.py
@@ -1,80 +1,51 @@
+from copy import deepcopy
 class Solution(object):
+    def pathTraverse(self, path, sec_path, prim_dir, sec_dir, i, startPos, k, max_fruits):
+        while abs(prim_dir - sec_dir) < k:
+            prim_dir += i
+            if prim_dir in path:
+                max_fruits += path[prim_dir]
+        curr_fruits = max_fruits
+        if prim_dir in path:
+            curr_fruits -= path[prim_dir]
+        while abs(sec_dir - startPos) <= k/2 and sec_dir >= 0:
+            if sec_dir != startPos:
+                prim_dir += -i
+                if prim_dir in path:
+                    curr_fruits -= path[prim_dir]
+            prim_dir += -i
+            if prim_dir in path:
+                curr_fruits -= path[prim_dir]
+            sec_dir += -i
+            if sec_dir in sec_path:
+                curr_fruits += sec_path[sec_dir]
+                del sec_path[sec_dir]
+            max_fruits = max(max_fruits, curr_fruits)
+        return max_fruits
+
     def maxTotalFruits(self, fruits, startPos, k):
-        """
-        :type fruits: List[List[int]]
-        :type startPos: int
-        :type k: int
-        :rtype: int
-        """
         max_fruits = 0
-        curr_fruits = 0
         right_path = {}
         left_path = {}
         right_pre = 0
         left_pre = 0
-        path = right_path
-        sec_path = left_path
-        left = [startPos]
-        right = [startPos]
-        prim_dir = right
-        sec_dir = left
-        i = 1
-
+        left = startPos
+        right = startPos
         for fruit in fruits:
             if fruit[0] > startPos:
                 if fruit[0] <= startPos + k:
                     right_path[fruit[0]] = fruit[1]
                     right_pre += fruit[1]
-
             elif fruit[0] == startPos:
                 max_fruits += fruit[1]
-
             else:
                 if fruit[0] >= startPos - k:
                     left_path[fruit[0]] = fruit[1]
                     left_pre += fruit[1]
-
-        if left_pre > right_pre:
-            path = left_path
-            sec_path = right_path
-            prim_dir = left
-            sec_dir = right
-            i = -1
-
         if (len(right_path) or len(left_path)):
-            while abs(left[0] - right[0]) < k and prim_dir[0] > 0:
-                prim_dir[0] += i
-                if prim_dir[0] in path:
-                    max_fruits += path[prim_dir[0]]
-
-            curr_fruits = max_fruits
-            print(left, right, max_fruits, curr_fruits)
-
-            if prim_dir[0] in path:
-                curr_fruits -= path[prim_dir[0]]
-
-            odd = 1
-            while abs(sec_dir[0] - startPos) <= k/2 and sec_dir[0] >= 0:
-                if odd != 1:
-                    print(left, right, max_fruits, curr_fruits)
-
-                    prim_dir[0] += -i
-                    if prim_dir[0] in path:
-                        curr_fruits -= path[prim_dir[0]]
-
-                prim_dir[0] += -i
-                if prim_dir[0] in path:
-                    curr_fruits -= path[prim_dir[0]]
-
-                sec_dir[0] += -i
-                
-                odd += 1
-                if sec_dir[0] in sec_path:
-                    curr_fruits += sec_path[sec_dir[0]]
-                    del sec_path[sec_dir[0]]
-                
-                max_fruits = max(max_fruits, curr_fruits)
-                print(left, right, max_fruits, curr_fruits)
-
-
+            right_max = (self.pathTraverse(right_path, deepcopy(left_path), right, left, 1, startPos, k, max_fruits))
+            left = startPos
+            right = startPos
+            left_max = (self.pathTraverse(left_path, right_path, left, right, -1, startPos, k, max_fruits))
+            max_fruits = max(left_max, right_max)
         return max_fruits
