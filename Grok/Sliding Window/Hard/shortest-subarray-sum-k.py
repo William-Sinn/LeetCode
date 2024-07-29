9ebc9f0ade3072f514b0544bd3c98e74bd0408f6
William-Sinn <sinnwilliam7@gmail.com>
2024-07-29 15:21:14 -0700

    Daily Problem 07/27/24 1 - First Approach (scrapped)

--- Grok/Sliding Window/Hard/shortest-subarray-sum-k.py
+++ Grok/Sliding Window/Hard/shortest-subarray-sum-k.py
@@ -1,3 +1,5 @@
+from heapq import heappush, heappop
+
 class Solution(object):
     def shortestSubarray(self, nums, k):
         """
@@ -5,4 +7,39 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+
+        num_heap = []
+        win_size = float('inf')
+        pref_array = []
+        pref_sum = 0
+        num_dict = {}
+
+        i = 0
+        for num in nums:
+            heappush(num_heap, -num)
+
+            pref_sum += num
+            pref_array.append(pref_sum)
+
+            num_dict[num] = i
+            i += 1
         
+        if pref_array[-1] < k:
+            return -1
+        elif pref_array == k:
+            return len(nums) 
+
+        while len(num_heap):
+            sum = 0
+            num = -heappop(num_heap)
+            curr_win = 1
+            left = right = num_dict[num]
+            pref_left = 0
+            pref_right = 0
+
+            while sum < k:
+                if curr_win >= win_size:
+                    break
+
+                if left -1 < 0 or 
+
