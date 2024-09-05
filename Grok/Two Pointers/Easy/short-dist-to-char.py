dac8785736de09f4f330ed5654bc7e22843959a2
William-Sinn <sinnwilliam7@gmail.com>
2024-09-05 16:34:43 -0700

    Daily Problem 09/05/24 2 - Problem End

--- Grok/Two Pointers/Easy/short-dist-to-char.py
+++ Grok/Two Pointers/Easy/short-dist-to-char.py
@@ -5,5 +5,22 @@ class Solution(object):
         :type c: str
         :rtype: List[int]
         """
-        
-        
+        p2 = 0
+        p3 = float('inf')
+        ans = []
+        s_len = len(s)
+
+        for p1 in range(s_len):
+            while p2 < s_len - 1 and s[p2] != c:
+                p2 += 1
+
+            if s[p2] == c:
+                ans.append(min(abs(p1 - p2), abs(p1 - p3)))
+            else:
+                ans.append(abs(p1 - p3))
+
+            if p2 == p1:
+                p3 = p1
+                p2 += 1
+                        
+        return ans
