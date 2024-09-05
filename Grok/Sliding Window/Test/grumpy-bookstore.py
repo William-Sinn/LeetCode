ad6f5725fa9b03a8fcf82f71b470090550876373
William-Sinn <sinnwilliam7@gmail.com>
2024-09-04 21:13:48 -0700

    Daily Problem 09/04/24 3 - Problem Start/End

---
+++ Grok/Sliding Window/Test/grumpy-bookstore.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def maxSatisfied(self, customers, grumpy, minutes):
+        current = 0
+        right = 0
+        left = 0
+        add = 0
+
+        while right - left < minutes:
+            if grumpy[right] == 1:
+                current += customers[right]
+            else:
+                add += customers[right]
+            right += 1
+        
+        out = current
+
+        while right < len(customers):
+            if grumpy[right] == 1:
+                current += customers[right]
+            else:
+                add += customers[right]
+            
+            if grumpy[left] == 1:
+                current -= customers[left]
+            
+            out = max(current, out)
+
+            right += 1
+            left += 1
+        
+        return out + add
+            
+            
