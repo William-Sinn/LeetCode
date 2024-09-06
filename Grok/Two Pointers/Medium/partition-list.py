decb4cd9b225b6f92c6c9fb0ca276d848e1c4319
William-Sinn <sinnwilliam7@gmail.com>
2024-09-05 21:09:06 -0700

    Daily Problem 09/05/24 4 - Problem Start

--- Grok/Two Pointers/Medium/partition-list.py
+++ Grok/Two Pointers/Medium/partition-list.py
@@ -1,13 +1,18 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
+class Solution:
     def partition(self, head, x):
-        """
-        :type head: ListNode
-        :type x: int
-        :rtype: ListNode
-        """
+        h1 = l1 = ListNode(0)
+        h2 = l2 = ListNode(0)
         
+        while head:
+            if head.val < x:
+                l1.next = head
+                l1 = l1.next
+            else:
+                l2.next = head
+                l2 = l2.next
+            head = head.next
+            
+        l2.next = None
+        l1.next = h2.next
+        
+        return h1.next
